d53f07afd7db829923aa3f4b84245c8c0d992b86
Vandhana <Vandhana@DESKTOP-TI919J8>
2022-01-24 17:12:10 +0530

    user1 first commit

---
+++ comcast.sdet29/src/test/java/demogit/Demogit.java
@@ -0,0 +1,15 @@
+package demogit;
+
+import org.testng.annotations.Test;
+
+public class Demogit {
+
+	@Test
+	public void demogit()
+	{
+		System.out.println("hi this is user1 first commit");
+	}
+	
+	
+	
+}
