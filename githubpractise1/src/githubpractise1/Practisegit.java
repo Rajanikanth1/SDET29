9a7289c0e6b1e14d5c2abf8b7b00294a8ad61336
Rajanikanth1 <rajanikanth355@gmail.com>
2022-01-29 19:28:25 +0530

    user 1 first commit

---
+++ githubpractise1/src/githubpractise1/Practisegit.java
@@ -0,0 +1,18 @@
+package githubpractise1;
+
+import org.testng.annotations.Test;
+
+public class Practisegit {
+	
+	
+@Test
+
+public void practisegit()
+{
+	System.out.println("user 1 first commit");
+	
+	}
+	
+	
+	
+}
