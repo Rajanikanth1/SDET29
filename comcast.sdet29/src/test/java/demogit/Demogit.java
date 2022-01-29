573a1c1e6ec1365d08f6ffecf0b0af0f8d1a26e6
Rajanikanth1 <rajanikanth355@gmail.com>
2022-01-29 18:56:07 +0530

    user 2 second commit

--- comcast.sdet29/src/test/java/demogit/Demogit.java
+++ comcast.sdet29/src/test/java/demogit/Demogit.java
@@ -12,6 +12,9 @@ public class Demogit {
 		System.out.println("hi this is user2 first commit");
 		
 		System.out.println("hi this is user1 second commit");
+		
+		System.out.println("hi this is user2 second commit");
+		
 	}
 	
 	
